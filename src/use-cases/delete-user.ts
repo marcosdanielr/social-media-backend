2ebbe2fe03cb8478b95ad83a0bbbd9d589320542
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 03:17:20 -0300

    FEAT: delete user

---
+++ src/use-cases/delete-user.ts
@@ -0,0 +1,30 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import {  User } from "@prisma/client";
+import { ResourceNotFound } from "./errors/resource-not-found-error";
+
+interface DeleteUserUseCaseRequest {
+  id: string;
+}
+
+interface DeleteUserUseCaseResponse {
+  user: User | null;
+}
+
+export class DeleteUserUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ id }: DeleteUserUseCaseRequest): Promise<DeleteUserUseCaseResponse> {
+
+        const userExists = await this.usersRepository.findById(id);
+
+        if (!userExists) {
+            throw new ResourceNotFound();
+        }
+
+        const user = await this.usersRepository.deleteById(id);
+
+        return {
+            user
+        };
+    }
+}
