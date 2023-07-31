ce5a78f18b033a32e9c23f3b40e3735894065648
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 14:06:54 -0300

    CHORE: title removed in post schema (migration)

---
+++ prisma/migrations/20230731170500_remove_title_of_post/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `posts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "posts" DROP COLUMN "title";
