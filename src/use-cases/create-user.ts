c39fe7496cfcb8b114ad4ce5296321f845f1eca8
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 01:08:24 -0300

    FEAT: create user use-case

---
+++ src/use-cases/create-user.ts
@@ -0,0 +1,31 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+interface CreateUserUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface CreateUserUseCaseResponse {
+  user: User;
+}
+
+export class CreateUserUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ name, email, password }: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
+        const password_hash = await hash(password, 6);
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            password_hash
+        });
+
+        return {
+            user
+        };
+    }
+}
