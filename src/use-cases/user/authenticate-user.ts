8ca00723dea8592d39ab7fbb813903272dfbdd1f
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 13:13:53 -0300

    FEAT: authenticate user (use-case)

---
+++ src/use-cases/user/authenticate-user.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+import { compare } from "bcryptjs";
+import { InvalidCredentialsError } from "../errors/invalid-credentials-error";
+
+interface AuthenticateUserUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateUserUseCaseResponse {
+  user: User;
+}
+
+export class AuthenticateUserUseCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ email, password }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new ResourceNotFoundError();
+        }
+
+        const doesPasswordMatches = await compare(password, user.password_hash);
+
+        if (!doesPasswordMatches) {
+            throw new InvalidCredentialsError();
+        }
+
+        return {
+            user
+        };
+    }
+}
