34966a061abab61fa8b67329a60baaa503536724
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 14:07:21 -0300

    CHORE: title removed in create-post (use-case)

--- src/use-cases/post/create-post.ts
+++ src/use-cases/post/create-post.ts
@@ -2,7 +2,6 @@ import { Post } from "@prisma/client";
 import { PostsRepository } from "@/repositories/posts-repository";
 
 interface CreatePostUseCaseRequest {
-  title: string;
   description: string;
   user_id: string;
 }
@@ -14,10 +13,9 @@ interface CreatePostUseCaseResponse {
 export class CreatePostUseCase {
     constructor(private postsRepository: PostsRepository) {}
 
-    async execute({ title, description, user_id }: CreatePostUseCaseRequest): Promise<CreatePostUseCaseResponse> {
+    async execute({ description, user_id }: CreatePostUseCaseRequest): Promise<CreatePostUseCaseResponse> {
         const post = await this.postsRepository.create({
             user_id,
-            title,
             description
         });
 
