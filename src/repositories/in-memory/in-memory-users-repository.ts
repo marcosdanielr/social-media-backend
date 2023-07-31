6c2b45758ee236e8e2fc8918073087891f5af5fd
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-31 01:01:58 -0300

    CHORE: user repository in memory

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,21 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { randomUUID } from "crypto";
+
+export class InMemoryUsersRepository implements UsersRepository {
+    public users: User[] = [];
+
+    async create(data: Prisma.UserCreateInput) {
+        const user = {
+            id: randomUUID(),
+            name: data.name,
+            email: data.email,
+            password_hash: data.password_hash,
+            created_at: new Date()    
+        };
+
+        this.users.push(user);
+
+        return user;
+    }
+}
