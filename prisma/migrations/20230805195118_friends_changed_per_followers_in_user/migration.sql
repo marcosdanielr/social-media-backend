86366843cdf3da6debea807df72ef4d8260a5e5b
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-05 16:52:02 -0300

    FEAT: followers migration, and friends removed

---
+++ prisma/migrations/20230805195118_friends_changed_per_followers_in_user/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_friends` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_friends" DROP CONSTRAINT "_friends_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_friends" DROP CONSTRAINT "_friends_B_fkey";
+
+-- DropTable
+DROP TABLE "_friends";
+
+-- CreateTable
+CREATE TABLE "Follows" (
+    "follower_id" TEXT NOT NULL,
+    "following_id" TEXT NOT NULL,
+
+    CONSTRAINT "Follows_pkey" PRIMARY KEY ("follower_id","following_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_follower_id_fkey" FOREIGN KEY ("follower_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_following_id_fkey" FOREIGN KEY ("following_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
