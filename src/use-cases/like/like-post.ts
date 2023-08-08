e0f412afde7ad424db353a24a5657e799b0b8b61
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-08 01:35:30 -0300

    FEAT: like post use case

---
+++ src/use-cases/like/like-post.ts
@@ -0,0 +1,14 @@
+import { LikesRepository } from "@/repositories/likes-repository";
+
+interface LikePostUseCaseRequest {
+  by_id: string;
+  post_id: string;
+}
+
+export class LikePostUseCase {
+  constructor(private likesRepository: LikesRepository) { }
+
+  async execute({ by_id, post_id }: LikePostUseCaseRequest): Promise<void> {
+    await this.likesRepository.likePost(by_id, post_id);
+  }
+} 
