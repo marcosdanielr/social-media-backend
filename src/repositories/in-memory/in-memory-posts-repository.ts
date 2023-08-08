569f4c2df0b153639b76e572fbec7d9fbf66e275
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-08 01:58:27 -0300

    CHORE: posts-repository types changed

--- src/repositories/in-memory/in-memory-posts-repository.ts
+++ src/repositories/in-memory/in-memory-posts-repository.ts
@@ -3,57 +3,57 @@ import { EditPost, PostsRepository } from "../posts-repository";
 import { randomUUID } from "node:crypto";
 
 export class InMemoryPostsRepository implements PostsRepository {
-    public posts: Post[] = [];
+  public posts: Prisma.PostUncheckedCreateInput[] = [];
 
-    async create(data: Prisma.PostUncheckedCreateInput) {
-        const post: Post = {
-            id: randomUUID(),
-            description: data.description,
-            user_id: data.user_id,
-            created_at: new Date(),
-            updated_at: new Date(),
-        };
+  async create(data: Prisma.PostUncheckedCreateInput) {
+    const post: Prisma.PostUncheckedCreateInput = {
+      id: randomUUID(),
+      description: data.description,
+      user_id: data.user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
 
-        this.posts.push(post);
+    this.posts.push(post);
 
-        return post;
+    return post;
+  }
+
+  async findById(id: string) {
+    const post = this.posts.find(item => item.id === id);
+
+    if (!post) {
+      return null;
     }
 
-    async findById(id: string) {
-        const post = this.posts.find(item => item.id === id);
+    return post;
+  }
 
-        if (!post) {
-            return null;
-        }
+  async edit({ id, user_id, data }: EditPost) {
+    const index = this.posts.findIndex(item => item.id === id && item.user_id === user_id);
+    const post: Prisma.PostUncheckedCreateInput = this.posts[index];
 
-        return post;
-    }
+    this.posts[index] = {
+      ...post,
+      description: data.description,
+      updated_at: new Date()
+    };
 
-    async edit({ id, user_id, data }: EditPost) {
-        const index = this.posts.findIndex(item => item.id === id && item.user_id === user_id);
-        const post: Post = this.posts[index];
+    return this.posts[index];
+  }
 
-        this.posts[index] = {
-            ...post,
-            description: data.description,
-            updated_at: new Date()
-        };
+  async deleteById(id: string) {
+    const index = this.posts.findIndex(item => item.id === id);
+    const post = this.posts[index];
 
-        return this.posts[index];
-    }
+    this.posts.splice(index, 1);
 
-    async deleteById(id: string) {
-        const index = this.posts.findIndex(item => item.id === id);
-        const post = this.posts[index];
+    return post;
+  }
 
-        this.posts.splice(index, 1);
+  async list(user_id: string, page: number, items_per_page: number = 20) {
+    const posts = this.posts.filter(item => item.user_id === user_id).slice((page - 1) * items_per_page, page * items_per_page);
 
-        return post;
-    }
-
-    async list(user_id: string, page: number, items_per_page: number = 20) {
-        const posts = this.posts.filter(item => item.user_id === user_id).slice((page - 1) * items_per_page, page * items_per_page);
-
-        return posts;
-    }
+    return posts;
+  }
 } 
