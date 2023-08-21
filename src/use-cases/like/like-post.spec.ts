2ed2bd5e06a76b5840c4cd46e56433c07a0bd27d
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-20 23:45:56 -0300

    TEST: like post

---
+++ src/use-cases/like/like-post.spec.ts
@@ -0,0 +1,40 @@
+import { it, describe, beforeEach, expect } from "vitest";
+import { LikePostUseCase } from "./like-post";
+import { InMemoryLikesRepository } from "@/repositories/in-memory/in-memory-likes-repository";
+import { InMemoryPostsRepository } from "@/repositories/in-memory/in-memory-posts-repository";
+
+let postsRepository: InMemoryPostsRepository;
+let likesRepository: InMemoryLikesRepository;
+let sut: LikePostUseCase;
+
+describe("Create Post Use Case", () => {
+    beforeEach(() => {
+        postsRepository = new InMemoryPostsRepository();
+        likesRepository = new InMemoryLikesRepository();
+        sut = new LikePostUseCase(likesRepository);
+    });
+
+    it.only("should be able to like a post", async () => {
+        const createPostResponse = await postsRepository.create({
+            user_id: "user-1",
+            description: "Oi"
+        });
+
+        await postsRepository.findById(createPostResponse.id);
+
+        await sut.execute({
+            by_id: "user-1",
+            post_id: createPostResponse.id
+        });
+
+        const likes = likesRepository.likes;
+
+        expect(likes.length).toEqual(1);
+        expect(likes).toEqual(
+            expect.objectContaining([{
+                by_id: "user-1",
+                post_id: createPostResponse.id
+            }])
+        );
+    });
+});
