eae14e658fbe26038e0a2456abafbabb275ce677
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-28 00:58:38 -0300

    FEAT: create user controller

---
+++ src/http/controllers/users/create.ts
@@ -0,0 +1,37 @@
+import { Status } from "@/http/status";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { makeCreateUserUseCase } from "@/use-cases/factories/users/make-create-user-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+    const bodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(), 
+        password: z.string().min(6)
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    try {
+        const createUserUseCase = makeCreateUserUseCase();
+
+        const { user } = await createUserUseCase.execute({
+            name,
+            email,
+            password
+        });
+
+        return reply.status(Status.CREATED).send({
+            ...user,
+            password_hash: undefined
+        });
+
+    } catch (error) {
+        if (error instanceof UserAlreadyExistsError) {
+            return reply.status(Status.CONFLICT).send({ message: error.message });
+        }
+
+        throw error;
+    }
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,4 +1,7 @@
 import { FastifyInstance } from "fastify";
 
+import { create } from "./create";
+
 export async function usersRoutes(app: FastifyInstance) {
+    app.post("/users", create);
 }
