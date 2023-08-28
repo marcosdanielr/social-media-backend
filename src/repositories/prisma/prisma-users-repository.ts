1605bdc6502143d5f0d6f6b26d30ab876b8ebbf5
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-28 00:34:42 -0300

    FEAT: create user in prisma-users-repository

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,24 @@
+import { Prisma } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { prisma } from "@/lib/prisma";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+
+export class PrismaUsersRepository implements UsersRepository {
+    async create(data: Prisma.UserCreateInput) {
+        const userExists = await prisma.user.findUnique({
+            where: {
+                email: data.email
+            }
+        });
+
+        if(userExists) {
+            throw new UserAlreadyExistsError();
+        }
+
+        const user = await prisma.user.create({
+            data
+        });
+
+        return user;
+    }
+}
