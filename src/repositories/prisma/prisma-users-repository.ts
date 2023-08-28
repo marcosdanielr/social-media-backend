cbbbb0e637c41305077082f28849fb75e1c0d9f3
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-28 02:21:42 -0300

    FEAT: find by id prisma-users-repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -46,6 +46,12 @@ export class PrismaUsersRepository implements UsersRepository {
     }
 
     async findById(id: string) {
-        return;      
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
+
+        return user;
     }
 }
