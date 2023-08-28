a05ecf5ae7cce0b97ae419f9071c046e022fe558
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-08-28 01:52:41 -0300

    FEAT: edit user controller

---
+++ src/http/controllers/users/edit.ts
@@ -0,0 +1,35 @@
+import { Status } from "@/http/status";
+import { makeEditUserUseCase } from "@/use-cases/factories/users/make-edit-user-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function edit(request: FastifyRequest, reply: FastifyReply) {
+    const bodySchema = z.object({
+        name: z.string().optional(),
+        email: z.string().email().optional(), 
+        password: z.string().min(6).optional()
+    }); 
+  
+    const paramsSchema = z.object({
+        userId: z.string().uuid()
+    }); 
+
+    const { name, email, password } = bodySchema.parse(request.body);
+    const { userId } = paramsSchema.parse(request.params);
+
+    const editUserUseCase = makeEditUserUseCase();
+
+    const user = await editUserUseCase.execute({
+        id: userId,
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+
+    reply.status(Status.OK).send({
+        ...user,
+        password: undefined
+    });
+} 
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
 
 import { create } from "./create";
+import { edit } from "./edit";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post("/users", create);
+    app.patch("/users/:userId", edit);
 }
